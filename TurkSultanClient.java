d83736e154fa7324b4e00668500f5d243d0d138f
benimgmailimaa-beep <benimgmailimaa@gmail.com>
2026-05-24 19:35:37 +0300

    Update TurkSultanClient.java

---
+++ TurkSultanClient.java
@@ -0,0 +1,11 @@
+package com.turksultan;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TurkSultanClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("TurkSultan Modu Başlatıldı!");
+    }
+}
+
